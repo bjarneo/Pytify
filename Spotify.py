ed6feeee9a4172b9dcb65bf2723a247d4e78d7ee
bjarneo <bjarne.oeverli@gmail.com>
2014-06-07 23:39:26 +0200

    Testing spotify API in python

---
+++ Spotify.py
@@ -0,0 +1,25 @@
+import sys
+import requests
+import time
+
+# Fetch songs with spotify api
+class Spotify:
+  url = 'https://ws.spotify.com/search/1/track.json?q='
+
+  # Get our data
+  def __init__(self, query):
+    response = requests.get(self.url + query)
+    self.data = response.json()
+  
+  # List all. Limit if needed
+  def list(self, limit = 100):
+    for key, song in enumerate(self.data['tracks']):
+      if (key == limit):
+        break
+
+      print str(key + 1) + ' ' + song['artists'][0]['name'] + ' - ' + song['name'] + ' :: ' + song['album']['name']
+      time.sleep(0.03)
+      
+  # Debug
+  def debug(self):
+    print self.data['tracks']
