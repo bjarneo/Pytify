df56d7d7ad48e5c727c923fb018ea277f9130b68
bjarneo <bjarne.oeverli@gmail.com>
2014-06-08 19:15:45 +0200

    Indentation fixed

--- Spotify.py
+++ Spotify.py
@@ -3,23 +3,23 @@ import time
 
 # Fetch songs with spotify api
 class Spotify:
-  _url = 'https://ws.spotify.com/search/1/track.json?q='
+    _url = 'https://ws.spotify.com/search/1/track.json?q='
 
-  # Get our data
-  def __init__(self, query):
-    response = requests.get(self._url + query)
-    self.data = response.json()
-  
-  # List all. Limit if needed
-  def list(self, limit = 100):
-    for key, song in enumerate(self.data['tracks']):
-      if (key == limit):
-        break
+    # Get our data
+    def __init__(self, query):
+        response = requests.get(self._url + query)
+        self.data = response.json()
 
-      print str(key + 1) + '. ' + song['artists'][0]['name'] + ' - ' + song['name'] + ' :: ' + song['album']['name']
-      # Sleeps just for the sexy output
-      time.sleep(0.03) 
-      
-  # Debug
-  def debug(self):
-    print self.data['tracks']
+    # List all. Limit if needed
+    def list(self, limit = 100):
+        for key, song in enumerate(self.data['tracks']):
+            if (key == limit):
+                break
+
+            print str(key + 1) + '. ' + song['artists'][0]['name'] + ' - ' + song['name'] + ' :: ' + song['album']['name']
+            # Sleeps just for the sexy output
+            time.sleep(0.03)
+
+    # Debug
+    def debug(self):
+        print self.data['tracks']
--- cli.py
+++ cli.py
@@ -2,10 +2,10 @@ import sys
 import Spotify as stfy
 
 try:
-  if (sys.argv[1]):
-    spotify = stfy.Spotify(sys.argv[1]) 
-    spotify.list(15)
+    if (sys.argv[1]):
+        spotify = stfy.Spotify(sys.argv[1])
+        spotify.list(15)
 except Exception, e:
     print e
-    print 'You need to add an argument.\n'\
+    print 'You need to add an argument.\n' \
           'Example python Spotify.py artist or Spotify.py "song name"'
