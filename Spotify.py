498aa4c6c86fc2d0feb605d0198762f52a09dab0
bjarneo <bjarne.oeverli@gmail.com>
2014-06-24 20:13:22 +0200

    dbus update

--- Spotify.py
+++ Spotify.py
@@ -1,23 +1,28 @@
 import requests
 import time
 import os
-
-
+import dbus
 # Fetch songs with spotify api
 class Spotify:
     _url = 'https://ws.spotify.com/search/1/track.json?q='
 
     # Get our data
     def __init__(self):
-        self.songs = {}
-        self.history = []
-        self.data = ''
+        self.__songs = {}
+        self.__history = []
+        self.__data = ''
+
+        session_bus = dbus.SessionBus()
+        self.spotify = dbus.Interface(
+            session_bus.get_object('org.mpris.MediaPlayer2.spotify', '/org/mpris/MediaPlayer2'),
+            'org.mpris.MediaPlayer2.Player'
+        )
 
     def search(self, query):
         if query:
             response = requests.get(self._url + query)
-            self.data = response.json()
-            self.history.append(query)
+            self.__data = response.json()
+            self.__history.append(query)
 
     # List all. Limit if needed
     def list(self, limit=100):
@@ -32,7 +37,7 @@ class Spotify:
             '-' * 30
         )
 
-        for key, song in enumerate(self.data['tracks']):
+        for key, song in enumerate(self.__data['tracks']):
             if key == limit:
                 break
 
@@ -44,7 +49,7 @@ class Spotify:
             )
 
             # Save spotify uri and song for later use
-            self.songs[key + 1] = {
+            self.__songs[key + 1] = {
                 'href': song['href'],
                 'song': '%s - %s' % (song['artists'][0]['name'].encode('utf-8'), song['name'].encode('utf-8'))
             }
@@ -53,33 +58,34 @@ class Spotify:
             time.sleep(0.01)
 
     def listen(self, index):
-        os.system('spotify ' + str(self.songs[index]['href']) + ' > /dev/null 2>&1')
+        os.system('spotify ' + str(self.__songs[index]['href']) + ' > /dev/null 2>&1')
 
-        return '\nPlaying: %s' % str(self.songs[index]['song'])
+        return '\nPlaying: %s\n' % str(self.__songs[index]['song'])
 
     def print_history(self):
-        if len(self.history) > 5:
-            self.history.pop(0)
+        if len(self.__history) > 5:
+            self.__history.pop(0)
 
         print '\nLast five search results:'
-        for song in self.history:
+        for song in self.__history:
             print song
 
     def next(self):
-        os.system('dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next >/dev/null')
+        self.spotify.Next()
 
     def prev(self):
-        os.system('dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous >/dev/null')
+        self.spotify.Prev()
 
     def play_pause(self):
-        os.system('dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause >/dev/null')
+        self.spotify.PlayPause()
 
     def stop(self):
-        os.system('dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop >/dev/null')
+        self.spotify.Stop()
 
     def meta(self):
+        # TODO: Fix metadata output
         os.system('dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:"org.mpris.MediaPlayer2.Player" string:"Metadata"')
 
     # Debug
     def debug(self):
-        print self.data['tracks']
+        print self.__data['tracks']
