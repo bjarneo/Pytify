b9f5b13ee644c2e4c17e42077e01ec25bb0ded4c
bjarneo <bjarne.oeverli@gmail.com>
2015-03-08 12:02:33 +0100

    cursers menu added

--- cli.py
+++ cli.py
@@ -2,13 +2,105 @@
 from spotipy import spotipy
 import argparse
 import sys
+import curses
+from curses import panel
+
+
+class Menu(object):
+    def __init__(self, items, stdscreen):
+        self.sptfy = spotipy.get_spotipy_class_by_platform()()
+        self.window = stdscreen.subwin(0,0)
+        self.window.keypad(1)
+        self.panel = panel.new_panel(self.window)
+        self.panel.hide()
+        panel.update_panels()
+
+        self.position = 2
+        self.items = items
+        self.items.append(' ')
+        self.items.append('<UP> and <DOWN> to navigate. <Enter> to select song. <Esc> for search.')
+        self.items.append('<LEFT> and <RIGHT> for prev/next song. <SPACEBAR> for play/pause.')
+
+    def navigate(self, n):
+        self.position += n
+
+        if self.position < 2:
+            self.position = 2
+        elif self.position > 16:
+            self.position = 16
+        elif self.position >= len(self.items):
+            self.position = len(self.items) - 1
+
+    def display(self):
+        self.panel.top()
+        self.panel.show()
+        self.window.clear()
+
+        while True:
+            self.window.refresh()
+            curses.doupdate()
+
+            for index, item in enumerate(self.items):
+                if index == self.position:
+                    mode = curses.A_REVERSE
+                else:
+                    mode = curses.A_NORMAL
+
+                self.window.addstr(index, 1, str(item), mode)
+
+            key = self.window.getch()
+
+            if key in [curses.KEY_ENTER, ord('\n')]:
+                if self.position == len(self.items) - 1:
+                    break
+                else:
+                    self.sptfy.listen(int(self.position - 1))
+
+            elif key == curses.KEY_UP:
+                self.navigate(-1)
+
+            elif key == curses.KEY_DOWN:
+                self.navigate(1)
+
+            elif key == curses.KEY_LEFT:
+                self.sptfy.prev()
+
+            elif key == curses.KEY_RIGHT:
+                self.sptfy.next()
+
+            # spacebar
+            elif key == 32:
+                self.sptfy.play_pause()
+
+            # escape
+            elif key == 27:
+                break
+
+        self.window.clear()
+        self.panel.hide()
+        panel.update_panels()
+        curses.doupdate()
 
 
 class App:
     def __init__(self):
         self.sptfy = spotipy.get_spotipy_class_by_platform()()
 
+        self.interaction()
+
         self.run()
+
+    def menu(self, list):
+        self.list = list
+
+        curses.wrapper(self.menu_items)
+
+    def menu_items(self, stdscreen):
+        curses.curs_set(0)
+
+        main_menu = Menu(self.list, stdscreen)
+        main_menu.display()
+
 
     def run(self):
         parser = argparse.ArgumentParser(description='Spotify remote')
@@ -33,7 +125,7 @@ class App:
             self.sptfy.stop()
 
         else:
-            self.intro()
+            #self.intro()
 
             self.interaction()
 
@@ -64,21 +156,8 @@ class App:
 
             if search_input:
                 self.sptfy.search(search_input)
-                self.sptfy.list(15)
-                self.sptfy.print_history()
 
-                if sys.version_info >= (3, 0):
-                    song_input = input('\nType song number and press <enter> to play. Press <enter> for new search.\n> ')
-                else:
-                    song_input = raw_input('\nType song number and press <enter> to play. Press <enter> for new search.\n> ')
-
-                if song_input:
-                    try:
-                        self.sptfy.listen(int(song_input))
-                    # stop user entering something that is not a number.
-                    except ValueError:
-                        continue
-
+                self.menu(list=self.sptfy.list())
 
 # Run the app
 if __name__ == '__main__':
