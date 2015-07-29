62bb5eef25aeab6ab9e6a7ca3a41880dd1ba6844
bjarneo <bjarne.oeverli@gmail.com>
2015-07-29 22:05:21 +0200

    move import inside of the method
    Just to no initialize anything before we do the platform check

--- pytify/strategy.py
+++ pytify/strategy.py
@@ -1,12 +1,13 @@
 from sys import platform
-from linuxpytify import LinuxPytify
-from darwinpytify import DarwinPytify
-
 
 def get_pytify_class_by_platform():
     if 'linux' in platform:
+        from linuxpytify import LinuxPytify
+
         return LinuxPytify
     elif 'darwin' in platform:
+        from darwinpytify import DarwinPytify
+
         return DarwinPytify
     else:
         raise Exception('%s is not supported.' % platform)
