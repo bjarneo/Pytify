336fc72ebb75ec8c8499154b0690d982fc4700dc
wohlfea <wohlfea@gmail.com>
2016-04-23 22:41:32 -0700

    Added absolute import for python 2 and 3.5 compatibility.

--- pytify/strategy.py
+++ pytify/strategy.py
@@ -1,12 +1,14 @@
+from __future__ import absolute_import
 from sys import platform
+
 
 def get_pytify_class_by_platform():
     if 'linux' in platform:
-        from linux import Linux
+        from pytify.linux import Linux
 
         return Linux
     elif 'darwin' in platform:
-        from darwin import Darwin
+        from pytify.darwin import Darwin
 
         return Darwin
     else:
