e05a0b152adb2afb04152c59d3c08cd4651876c7
Bharath Vedartham <linux.bhar@gmail.com>
2019-10-03 02:22:30 +0530

    Add prompt_toolkit v2 support in Pytify
    prompt_toolkit v2 had a lot of breaking changes which caused Pytify to
    crash when prompt_toolkit v2 was installed. This is an attempt to make
    appropriate changes in Pytify to ensure Pytify does not break when
    prompt_toolkit v2 is installed.
    
    The following changes were made:
    (i) PromptSession is used instead of prompt. This is the standard for
    prompt_toolkit v2
    
    (ii) AbortAction is removed in prompt_toolkit v2. Manual checks for
    KeyboardInterrupt and EOF have to be done. on_abort parameter is removed
    from the prompt function.
    
    (iii) FileHistory does not have iter magic function anymore. a
    load_history_strings() functions is provided which returns the list of
    strings in the History file.
    
    (iv) Token class is removed in v2. Styling of prompt is done by
    pygments. Style class is used from prompt_toolkit.styles to define the
    styling of the prompt.
    
    (v) complete_in_thread is added to PromptSession to enable asynchronous
    completion of text.
    
    (vi) WordCompleter accepts a list of words rather than a set of words. A
    list of words is passed as the first param to WordCompleter rather thana
    set.
    
    (vii) get_prompt_tokens and get_bottom_toolbar_tokens were renamed to
    get_prompt and get_bottom. This is because tokens are not
    supported anymore by prompt_toolkit, the naming should be appropriate to
    reflect this.
    
    This code was tested by running all the possible commands.

--- pytify/prompt.py
+++ pytify/prompt.py
@@ -1,64 +1,69 @@
 from __future__ import absolute_import, unicode_literals
 import getpass
 import os
-from prompt_toolkit import prompt, AbortAction
+from prompt_toolkit import PromptSession
 from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
-from prompt_toolkit.token import Token
-from prompt_toolkit.styles import style_from_dict
-from prompt_toolkit.contrib.completers import WordCompleter
+from prompt_toolkit.formatted_text import PygmentsTokens
+from prompt_toolkit.styles import Style
+from prompt_toolkit.completion import WordCompleter, ThreadedCompleter
 from pytify.history import history
 
-
-style = style_from_dict({
-    Token.Username:  '#84bd00 italic',
-    Token.At:        '#999999',
-    Token.Host:      '#84bd00',
-    Token.Separator: '#84bd00',
-    Token.Text:      '#e6e6e6',
-    Token.Arrow:     '#999999',
-    Token.SelectedText: 'reverse underline',
-    Token.Toolbar: '#e6e6e6 bg:#262626',
+style = Style.from_dict({
+    'username':  '#84bd00 italic',
+    'at':        '#999999',
+    'host':      '#84bd00',
+    'separator': '#84bd00',
+    'text':      '#e6e6e6',
+    'arrow':     '#999999',
+    'selectedtext': 'reverse underline',
+    'toolbar': '#e6e6e6 bg:#262626',
 })
 
 
 def completer():
-    list = []
+    s = set()
 
-    for name in history():
-        list.append(name)
+    history_strings = history().load_history_strings()
 
-    return WordCompleter(set(list), ignore_case=True)
+    for name in history_strings:
+        s.add(name)
+
+    return WordCompleter(list(s), ignore_case=True)
 
 
-def get_bottom_toolbar_tokens(currentSong):
-    def toolbar(cli):
+def get_bottom_toolbar(currentSong):
+    def toolbar():
         return [
-            (Token.Toolbar, ' exit: ctrl+d | clear: ctrl+c | song: %s' % currentSong)
+            ('class:toolbar', ' exit: ctrl+d | clear: ctrl+c | song: %s' % currentSong)
         ]
 
     return toolbar
 
 
-def get_prompt_tokens(cli):
+def get_prompt():
     return [
-        (Token.Username,  getpass.getuser()),
-        (Token.At,        '@'),
-        (Token.Host,      os.uname()[1]),
-        (Token.Separator, ' - '),
-        (Token.Text,      'Search:'),
-        (Token.Arrow,     '\n> '),
+        ('class:username',  getpass.getuser()),
+        ('class:at',        '@'),
+        ('class:host',      os.uname()[1]),
+        ('class:seperator', ' - '),
+        ('class:text',      'Search:'),
+        ('class:arrow',     '\n> '),
     ]
 
 
-def custom_prompt(currentSong):
-    return prompt(
-        get_prompt_tokens=get_prompt_tokens,
+def custom_prompt(currentSong): 
+    
+    session = PromptSession(
+        message=get_prompt,
         history=history(),
         auto_suggest=AutoSuggestFromHistory(),
         enable_history_search=True,
-        on_abort=AbortAction.RETRY,
-        get_bottom_toolbar_tokens=get_bottom_toolbar_tokens(currentSong),
+        bottom_toolbar=get_bottom_toolbar(currentSong),
         completer=completer(),
         complete_while_typing=True,
+        complete_in_thread=True,
         style=style
     )
+
+    return session.prompt()
+
